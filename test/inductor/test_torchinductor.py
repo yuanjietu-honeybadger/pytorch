24044920460bda56424a8d96fbee74b4c628d9a3
Boyuan Feng <boyuan@meta.com>
2026-04-15 11:20:17 -0700

    nit

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -13986,6 +13986,7 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
         fn(a, b)
 
     # Skipped on ROCm until https://github.com/ROCm/triton/issues/443 resolved
+    @xfail_if_mps  # Metal has a 31-buffer argument limit per kernel
     @slowTest
     def test_fuse_large_params(self):
         def pt2_optimizer_step(optimizer):
